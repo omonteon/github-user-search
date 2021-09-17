b231fcdbb272205864d6f2406659e6b8fa8fb64b
Omar Monteon <monteon.omar@gmail.com>
2021-09-17 12:58:18 -0500

    Feat: Adding logic, markup and styles for top info

--- src/components/GitHubProfile.js
+++ src/components/GitHubProfile.js
@@ -1,7 +1,44 @@
 import React from "react";
 
 function GitHubProfile({ user = null }) {
-  return <div>Github profile</div>;
+  const MONTHS = [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec",
+  ];
+  if (user === null) {
+    return null;
+  }
+  function getJoinedAtDate(dateStr) {
+    const date = new Date(dateStr);
+    const day = date.getDate();
+    const month = MONTHS[date.getMonth()];
+    const year = date.getFullYear();
+    return `${day} ${month} ${year}`;
+  }
+  return (
+    <div className="github-profile">
+      <div className="top-info">
+        <img src={user.avatar_url} alt="profile photo" />
+        <div className="top-info__text-data">
+          <h2>{user.name}</h2>
+          <span className="user-handle">@{user.login}</span>
+          <span className="joined-at">
+            Joined {getJoinedAtDate(user.created_at)}
+          </span>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default GitHubProfile;
