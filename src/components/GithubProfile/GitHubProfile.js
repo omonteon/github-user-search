22092463cdb6b0564d0a1da512f63283b27dd099
Omar Monteon <monteon.omar@gmail.com>
2022-09-09 19:46:06 -0500

    refactor: moved styles to css module

--- src/components/GithubProfile/GitHubProfile.js
+++ src/components/GithubProfile/GitHubProfile.js
@@ -5,6 +5,7 @@ import { ReactComponent as WebSiteIcon } from "../../assets/icon-website.svg";
 import { ReactComponent as CompanyIcon } from "../../assets/icon-company.svg";
 import { ReactComponent as TwitterIcon } from "../../assets/icon-twitter.svg";
 import { MONTHS } from "../../constants";
+import styles from "./GithubProfile.module.css";
 
 function GitHubProfile({ user = null }) {
   if (user === null) {
@@ -18,31 +19,33 @@ function GitHubProfile({ user = null }) {
     return `${day} ${month} ${year}`;
   }
   return (
-    <div className="github-profile">
-      <img src={user.avatar_url} alt={user.name} className="avatar" />
-      <div className="top-info">
-        <div className="top-info__text-data">
+    <div className={styles["github-profile"]}>
+      <img src={user.avatar_url} alt={user.name} className={styles.avatar} />
+      <div className={styles["top-info"]}>
+        <div className={styles["top-info__text-data"]}>
           <h2>{user.name}</h2>
-          <p className="user-handle">@{user.login}</p>
-          <p className="joined-at">Joined {getJoinedAtDate(user.created_at)}</p>
+          <p className={styles["user-handle"]}>@{user.login}</p>
+          <p className={styles["joined-at"]}>
+            Joined {getJoinedAtDate(user.created_at)}
+          </p>
         </div>
       </div>
       {user.bio ? <p className="bio">{user.bio}</p> : null}
-      <ul className="metrics">
-        <li className="metric">
+      <ul className={styles["metrics"]}>
+        <li className={styles["metric"]}>
           <h3>Repos</h3>
           <p>{user.public_repos}</p>
         </li>
-        <li className="metric">
+        <li className={styles["metric"]}>
           <h3>Followers</h3>
           <p>{user.followers}</p>
         </li>
-        <li className="metric">
+        <li className={styles["metric"]}>
           <h3>Following</h3>
           <p>{user.following}</p>
         </li>
       </ul>
-      <ul className="bottom-info">
+      <ul className={styles["bottom-info"]}>
         <li>
           <SingleLineDataWithIcon data={user.location}>
             <LocationIcon width={20} />
