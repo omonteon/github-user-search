d77a6897621a57d14eba8cf471569699817ffc94
Omar Monteon <monteon.omar@gmail.com>
2022-09-09 19:39:29 -0500

    refactor: moved styles to css module

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect, useRef } from "react";
+import styles from "./SearchBar.module.css";
 
 function SearchBar({ onSearch, error = false, onTextChange }) {
   const [searchText, setSearchText] = useState("");
@@ -17,11 +18,11 @@ function SearchBar({ onSearch, error = false, onTextChange }) {
     onSearch(searchText);
   }
   return (
-    <form className="search-bar" onSubmit={submitForm}>
+    <form className={styles["search-bar"]} onSubmit={submitForm}>
       <label htmlFor="inputSearch" className="sr-only">
         Enter githhub username
       </label>
-      <div className="input-container">
+      <div className={styles["input-container"]}>
         <input
           ref={inputRef}
           type="text"
@@ -35,7 +36,7 @@ function SearchBar({ onSearch, error = false, onTextChange }) {
           }}
         />
       </div>
-      {error ? <p className="error-message">No results</p> : null}
+      {error ? <p className={styles["error-message"]}>No results</p> : null}
       <button type="submit" disabled={searchText === "" ? "disabled" : false}>
         Search
       </button>
