f45bc1913b8baf92a1b5c0c5c78723d411de0395
Omar Monteon <monteon.omar@gmail.com>
2022-09-26 19:32:12 -0500

    test: added new unit test for profile

--- src/components/GithubProfile/GithubProfile.spec.js
+++ src/components/GithubProfile/GithubProfile.spec.js
@@ -1,7 +1,15 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import GithubProfile from "./GithubProfile";
+import { GITHUB_PROFILE } from "../../constants/testData";
+
+test("should render null Github Profile", () => {
+  render(<GithubProfile />);
+});
 
 test("should render the Github Profile", () => {
-  render(<GithubProfile />);
+  render(<GithubProfile user={GITHUB_PROFILE} />);
+
+  const userHandle = screen.getByText(`@${GITHUB_PROFILE.login}`);
+  expect(userHandle).toBeInTheDocument();
 });
